6f2b5624c9741d57f65a6a74f2f8037eb396a27e
ghozt777 <ghozt777.code@gmail.com>
2022-06-02 02:29:47 +0530

    console log removed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,6 @@ import * as cookieParser from 'cookie-parser';
 import 'reflect-metadata'
 
 async function bootstrap() {
-  console.log('debug: ', process.env.DB_HOST);
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   await app.listen(process.env.PORT || 5001);
