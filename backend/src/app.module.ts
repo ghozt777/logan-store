7a8fb35d59d9ddd989eb48c2ca26bbcea7d2dce2
ghozt777 <ghozt777.code@gmail.com>
2022-06-15 02:05:54 +0530

    backend server security update

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,6 +10,7 @@ import { ImageModule } from './images/image.module';
 import { AddressModule } from './address/address.module';
 import { CategoryModule } from './entity-category/category.module';
 import { ProductCategoryModule } from './product-category/productCategory.module';
+import { __prod__ } from './config';
 
 
 @Module({
@@ -28,7 +29,9 @@ import { ProductCategoryModule } from './product-category/productCategory.module
           cookies: req.cookies ?? []
         }
       },
-      cors: { origin: true, credentials: true }
+      cors: { origin: true, credentials: true },
+      playground: !__prod__, // security purposes -> read : https://www.apollographql.com/docs/apollo-server/v2/testing/graphql-playground/
+      introspection: !__prod__ // security purposes -> read : https://hasura.io/learn/graphql/intro-graphql/introspection/
     }),
     DatabaseModule,
     UserModule,
